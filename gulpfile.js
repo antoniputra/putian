747f8459db2f3f513c3683914dbb23e790e6c8a0
Antoni Putra <akiddcode@gmail.com>
2015-11-09 19:41:20 +0700

    initial, folder structure, setup task runner gulp, and dummy index.html

---
+++ gulpfile.js
@@ -0,0 +1,89 @@
+/*!
+* gulp. (do below command first)
+* $ npm install gulp-ruby-sass gulp-autoprefixer gulp-minify-css gulp-jshint gulp-concat gulp-uglify gulp-imagemin gulp-notify gulp-rename gulp-livereload gulp-cache del --save-dev
+*/
+
+// Load plugins
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    del = require('del'),
+    sourcemaps = require('gulp-sourcemaps'),
+    csslint = require('gulp-csslint');
+
+// Styles
+gulp.task('styles', function() {
+    return sass('src/styles/app.scss', { style: 'expanded' })
+        .pipe(autoprefixer('last 2 version'))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(minifycss())
+        .pipe(gulp.dest('dist/css'))
+        .pipe(notify({ message: 'Styles task complete prend' }));
+});
+
+// Scripts
+gulp.task('scripts', function() {
+    return gulp.src(['bower_components/jquery/dist/jquery.js', 'bower_components/bootstrap-sass/assets/javascripts/bootstrap.js', 'src/scripts/app.js'])
+        // .pipe(jshint('.jshintrc'))
+        // .pipe(jshint.reporter('default'))
+        .pipe(concat('app.js'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist/js'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'))
+        .pipe(notify({ message: 'Scripts task complete prend' }));
+});
+
+// Images
+gulp.task('images', function() {
+    return gulp.src('src/images/**/*')
+        .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+        .pipe(gulp.dest('dist/images'))
+        .pipe(notify({ message: 'Images task complete' }));
+});
+
+// Lint
+// gulp.task('lint', function() {
+//     return gulp.src('client/css/*.css')
+//         .pipe(csslint())
+//         .pipe(csslint.reporter());
+// });
+
+// Clean
+gulp.task('clean', function(cb) {
+    del(['dist/css', 'dist/js', 'dist/img'], cb)
+});
+
+// Default task
+gulp.task('default', ['clean', 'styles', 'scripts', 'images']);
+
+// Watch
+gulp.task('watch', function() {
+
+    // Watch .scss files
+    gulp.watch('src/styles/**/*.scss', ['styles']);
+
+    // Watch .js files
+    gulp.watch('src/scripts/**/*.js', ['scripts']);
+
+    // Watch image files
+    gulp.watch('src/images/**/*', ['images']);
+
+    // Create LiveReload server
+    livereload.listen();
+
+    // Watch any files in dist/, reload on change
+    gulp.watch(['dist/**']).on('change', livereload.changed);
+
+});
